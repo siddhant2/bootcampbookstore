971fcca7d0bba5cdaacc89b00cf57489727d507e
siddhant2 <31253947+siddhant2@users.noreply.github.com>
2020-07-19 13:15:24 +0530

    Delete UserShippingService.java

--- UserShippingService.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.service;
-
-import com.bookstore.domain.UserShipping;
-
-public interface UserShippingService {
-    UserShipping findById(Long id);
-
-    void removeById(Long id);
-}
