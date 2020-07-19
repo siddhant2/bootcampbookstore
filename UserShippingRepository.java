879669d8ef5d28c0cc4a462f603d5fb2433e569a
siddhant2 <31253947+siddhant2@users.noreply.github.com>
2020-07-19 13:06:23 +0530

    Delete UserShippingRepository.java

--- UserShippingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.repository;
-
-import com.bookstore.domain.UserShipping;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserShippingRepository extends JpaRepository<UserShipping, Long> {
-
-}
